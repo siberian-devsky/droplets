8f41ba79aafcf89c764ac7e3c04d01c10a319eb1
Jeff Martin <chess.logic@gmail.com>
2025-10-15 17:41:34 -0700

    css fuckery

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const droplets = document.querySelectorAll(".droplet");
 const mainWindowCoords = mainWindow.getBoundingClientRect();
 const dropletHeight = 45;
 
-function createDrop() {
+function createDroplet() {
    // just the divs
     const droplet = document.createElement("div");
     const head = document.createElement("div");
 
     const tails = [];
-    const randomTailLength = Math.floor(Math.random() * 10) //max 10
+    const randomTailLength = Math.floor(Math.random() * 10) + 3 // 3-12
 
     for (let i = 0; i <= randomTailLength; i++) {
         const tail = document.createElement("div");
@@ -25,39 +25,53 @@ function createDrop() {
     // assemble
     tails.forEach( tail => droplet.appendChild(tail));
     droplet.appendChild(head);
-
-    //random position inside the mainWindow
+    
+    // random position inside the mainWindow
     const width = mainWindow.clientWidth - 10;
     const xPos = Math.floor(Math.random() * width)
-
+    
+    // starting line
     droplet.style.left = `${xPos}px`;
-    droplet.style.transform = "translateY(-25px)";
+    droplet.style.transform = "translateY(-125px)";
     droplet.style.position = "absolute";
-
-    // ship it
+    
+    // attach to DOM
     mainWindow.appendChild(droplet)
+
     requestAnimationFrame( () => {
-        mainWindow.clientHeight + 40
-        droplet.style.transition = `transform ${getRandomFallRate()}s linear`;
+        const transitionProps = getTransitionProps(tails.length);
+        droplet.style.transition = `transform ${transitionProps.fallRate}s ${transitionProps.timingFunc}`;
+
+        // finish line
         droplet.style.transform = `translateY(${mainWindowCoords.bottom + dropletHeight}px)`;
     })
     
     // kill it when out of frame
-    setTimeout(
-        droplet.addEventListener("transitionend", () => {
-            droplet.remove()
-    }), 5000)
+    droplet.addEventListener("transitionend", () => droplet.remove)
 }
 
 function fallDown() {
-    console.log("falling");
     setInterval(() => {
-        createDrop();
-    }, 1000);
+        createDroplet();
+    }, 75);
 }
 
-function getRandomFallRate() {
-      return (Math.random() * 3 + 1).toFixed(2); // 1s–4s
+function getTransitionProps(tailLength) {
+    const transition = {
+        fallRate: ((Math.random() * 5) + 2).toFixed(2), // 1s–4s
+        timingFunc: tailLength % 2 === 0 ? "ease" : "linear"
+    }
+
+    if (typeof transition.fallRate === Number
+        && typeof timingFunc === "ease" || "linear"
+    ) {
+        return transition
+    } else {
+        return {
+            fallRate: 3,
+            timingFunc: "ease"
+        }
+    }
 }
 
-mainWindow.addEventListener("click", fallDown);
+document.addEventListener("DOMContentLoaded", fallDown);
